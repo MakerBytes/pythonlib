1784eaca770cb8cd0ba917c7e94bbaf3b5e063f0
Joshua Lowe <joshualowe1002@gmail.com>
2020-04-20 14:19:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='joshbasiccalculator',
+  version='0.0.1',
+  description='A very basic calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Joshua Lowe',
+  author_email='josh@edublocks.org',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
