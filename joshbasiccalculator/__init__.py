a68c59915f5b33118a3947bc3b791ed09254be76
Joshua Lowe <joshualowe1002@gmail.com>
2020-04-20 14:20:58 +0100

    Create __init__.py

---
+++ joshbasiccalculator/__init__.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
